d14ada16ba49461f4c854d2e297a6e4ffb12ce28
elainetneoh <63639461+elainetneoh@users.noreply.github.com>
2020-06-11 19:26:58 +0800

    Update MainController.java

--- src/com/myapp/servlets/MainController.java
+++ src/com/myapp/servlets/MainController.java
@@ -34,7 +34,7 @@ public class MainController extends HttpServlet {
 		}
 //		response.getWriter().append("Hello "+uname+" this wepapp is served at Served at: ").append(request.getContextPath());
         
-		String message="Hello dear "+uname+" You have succesffully loging in ";
+		String message="Hello Customer "+uname+" You have successfully logging in ";
 		request.setAttribute("message", message);
         request.getRequestDispatcher("/Welcome.jsp").forward(request, response);
 	
